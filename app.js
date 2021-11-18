9564bd2c6d15cfd4a3fb1766346b0471d3faca40
FredHorizon <fredoliveira.sys@gmail.com>
2021-11-17 21:21:58 -0400

    Cálculo de tempo restante no tempo corrente

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const nextYear = new Date().getFullYear() + 1
 const newYearTime = new Date(`January 01 ${nextYear} 00:00:00`)
 
-const updateContdown = () => {
+const updateCountdown = () => {
   const currentTime = new Date()
 
   // Obter a diferença entre a zero hora do primeiro dia do ano que vem e a data atual
@@ -9,16 +9,16 @@ const updateContdown = () => {
   const difference = newYearTime - currentTime
 
   // Cálculo do tempo restante que falta para o dia 01 do ano que vem chegar
-  // Em SEGUNDOS
-  const seconds = Math.floor(difference / 1000)
-  // Em MINUTOS
-  const minutes = Math.floor(difference / 1000 / 60)
-  // Em HORAS (Mas levar em conta as horas que já se passaram nesse momento)
-  const hours = Math.floor(difference / 1000 / 60 / 60)
-  // Em DIAS
+  // Em DIAS para o no atual acabar
   const days = Math.floor(difference / 1000 / 60 / 60 / 24)
+  // Em HORAS para o dia corrente (de hoje, de agora) acabar
+  const hours = Math.floor(difference / 1000 / 60 / 60) % 24
+  // Em MINUTOS para a hora corrente acabar
+  const minutes = Math.floor(difference / 1000 / 60) % 60
+  // Em SEGUNDOS para o minuto corrente acabar
+  const seconds = Math.floor(difference / 1000) % 60
 
-  console.log(days)
+  console.log(days, hours, minutes, seconds)
 }
 
-updateContdown()
+updateCountdown()
