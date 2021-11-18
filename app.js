a54854843559a330d2ce92f9c6e923166201dd2e
FredHorizon <fredoliveira.sys@gmail.com>
2021-11-18 14:37:32 -0400

    Refatora o projeto

--- app.js
+++ app.js
@@ -9,35 +9,34 @@ const countDownContainer = document.querySelector('#countdown')
 const nextYear = new Date().getFullYear() + 1
 const newYearTime = new Date(`January 01 ${nextYear} 00:00:00`)
 
+const getTimeUnit = unit => unit < 10 ? '0' + unit : unit
+
 nextYearContainer.textContent = nextYear
+
+const insertCountdownValues = ({ days, hours, minutes, seconds }) => {
+  secondsContainer.textContent = getTimeUnit(seconds)
+  minutesContainer.textContent = getTimeUnit(minutes)
+  hoursContainer.textContent = getTimeUnit(hours)
+  daysContainer.textContent = getTimeUnit(days)
+}
 
 const updateCountdown = () => {
   const currentTime = new Date()
-
-  // Obter a diferença entre a zero hora do primeiro dia do ano que vem e a data atual
-  // O resulta será a quantidade de milisegundos que restam para a zero hora do primeiro dia do ano que vem.
   const difference = newYearTime - currentTime
 
-  // Cálculo do tempo restante que falta para o dia 01 do ano que vem chegar
-  // Em DIAS para o no atual acabar
   const days = Math.floor(difference / 1000 / 60 / 60 / 24)
-  // Em HORAS para o dia corrente (de hoje, de agora) acabar
   const hours = Math.floor(difference / 1000 / 60 / 60) % 24
-  // Em MINUTOS para a hora corrente acabar
   const minutes = Math.floor(difference / 1000 / 60) % 60
-  // Em SEGUNDOS para o minuto corrente acabar
   const seconds = Math.floor(difference / 1000) % 60
 
-  secondsContainer.textContent = seconds < 10 ? '0' + seconds : seconds // ternário para que continue com 2 dígitos
-  minutesContainer.textContent = minutes < 10 ? '0' + minutes : minutes
-  hoursContainer.textContent = hours < 10 ? '0' + hours : hours
-  daysContainer.textContent = days < 10 ? '0' + days : days
+  insertCountdownValues({ days, hours, minutes, seconds })
 }
 
-setTimeout(() => {
+handleCountdownDisplay = () => {
   spinnerLoading.remove()
   countDownContainer.style.display = 'flex'
-}, 1000)
+}
 
-// Invoca a função a cada segundo
+setTimeout(handleCountdownDisplay, 2000)
+
 setInterval(updateCountdown, 1000)
